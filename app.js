13cf72e08de0ca1359503b7b371e2de1eb455569
panutat-p <pp.panutat@gmail.com>
2021-11-03 14:25:22 +0700

    complete tiebreaker

--- app.js
+++ app.js
@@ -16,11 +16,15 @@ const matchPointBtn = document.querySelector('#match-point');
 
 const resetBtn = document.querySelector('#reset-btn');
 
-player1.winnerFlag.style.display = 'none';
-player2.winnerFlag.style.display = 'none';
+const matchPointLabel = document.querySelector('#match-point-label');
 
 let matchPoint = 3;
+let previousMatchPoint = matchPoint;
 let isGameOver = false;
+
+player1.winnerFlag.style.display = 'none';
+player2.winnerFlag.style.display = 'none';
+matchPointBtn.value = matchPoint;
 
 const disableBothBtn = (p1, p2) => {
   if (isGameOver) {
@@ -29,16 +33,31 @@ const disableBothBtn = (p1, p2) => {
   }
 };
 
-const updateScores = (player) => {
+const changeToTiebreaker = (p1, p2) => {
+  matchPointLabel.innerText = "Tiebreaker";
+  previousMatchPoint = matchPoint;
+  matchPoint = 2;
+  matchPointBtn.value = matchPoint;
+  for (let p of [p1, p2]) {
+    p.score = 0;
+    p.display.textContent = 0;
+  }
+}
+
+const updateScores = (player, opponent) => {
   player.score += 1;
   player.display.textContent = player.score;
   if (player.score === matchPoint) {
     isGameOver = true;
     player.winnerFlag.style.display = 'block';
+  } else if (player.score === (matchPoint - 1) && player.score === opponent.score) {
+    console.log("tie breaker");
+    changeToTiebreaker(player, opponent);
   }
 };
 
 const resetScores = (p1, p2) => {
+  matchPointLabel.innerText = "PlayingTo";
   isGameOver = false;
   for (let p of [p1, p2]) {
     p.score = 0;
@@ -56,15 +75,17 @@ matchPointBtn.addEventListener('change', () => {
 });
 
 player1.button.addEventListener('click', () => {
-  updateScores(player1);
+  updateScores(player1, player2);
   disableBothBtn(player1, player2);
 });
 
 player2.button.addEventListener('click', () => {
-  updateScores(player2);
+  updateScores(player2, player1);
   disableBothBtn(player1, player2);
 });
 
 resetBtn.addEventListener('click', () => {
+  matchPoint = previousMatchPoint;
+  matchPointBtn.value = matchPoint;
   resetScores(player1, player2);
 });
